d708e6feee28647d171ca608bade4570b324435b
Felipe Tozadori Silva <ftozadori@gmail.com>
2025-06-03 17:03:19 -0300

    aa

---
+++ Aula01AutomacaoDeTarefas/codigo.py
@@ -0,0 +1,9 @@
+# Passo 1: Entrar no sistema da empresa - https://dlp.hashtagtreinamentos.com/python/intensivao/login
+# Passo 2: Fazer login
+# Passo 3: Importar a base de dados
+# Passo 4: Cadastrar um produto
+# Passo 5: Repetir para todos os produtos 
+
+#pyautogui -> fazer automações com phyton
+
+import pyautogui
